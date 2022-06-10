f9467a591fa94774a72ef712619a4c75dd7084a4
AllenAlexander98 <73653888+AllenAlexander98@users.noreply.github.com>
2022-06-10 10:18:51 +0700

    building natours api

---
+++ app.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
+const { create } = require('domain');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// MIDDLEWARES
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
